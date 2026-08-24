d76043bcdd5c50011a4e49e258e61fb714e1968a
Pierre du Plessis <pierre@pcservice.co.za>
2026-08-24 12:10:25 +0200

    Add data collector to show tenant information in toolbar

---
+++ src/Bundle/Platform/DataCollector/TenantDataCollector.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace SolidWorx\Platform\PlatformBundle\DataCollector;
+
+use SolidWorx\Platform\PlatformBundle\Model\Tenant;
+use SolidWorx\Platform\PlatformBundle\Repository\TenantRepository;
+use SolidWorx\Platform\PlatformBundle\Repository\UserTenantRepository;
+use SolidWorx\Platform\PlatformBundle\Tenant\TenantContext;
+use Symfony\Bundle\FrameworkBundle\DataCollector\AbstractDataCollector;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\VarDumper\Cloner\Data;
+
+#[AutoconfigureTag(
+    name: 'data_collector',
+    attributes: [
+        'id' => self::COLLECTOR_ID,
+        'priority' => -255,
+    ],
+)]
+final class TenantDataCollector extends AbstractDataCollector
+{
+    private const string COLLECTOR_ID = 'tenant';
+    private const string COLLECTOR_TEMPLATE = '@Platform/DataCollector/tenant.html.twig';
+
+    public function __construct(
+        private readonly TenantContext    $tenantContext,
+        private readonly TenantRepository $tenantRepository,
+        private readonly UserTenantRepository $userTenantRepository,
+        private readonly Security $security,
+    ) {}
+
+    public function collect(Request $request, Response $response, ?\Throwable $exception = null): void
+    {
+        $tenantId = $this->tenantContext->getTenantId();
+        $this->data = [
+            'tenant' => $tenantId ? $this->tenantRepository->find($tenantId) : null,
+            'user_tenants' => $this->userTenantRepository->findTenantsForUser($this->security->getUser()?->getId()),
+        ];
+    }
+
+    public function getName(): string
+    {
+        return self::COLLECTOR_ID;
+    }
+
+    public function getTenant(): ?Tenant
+    {
+        return $this->data['tenant'] ?? null;
+    }
+
+    public function getTenantData(): ?Data
+    {
+        return isset($this->data['tenant']) ? $this->cloneVar($this->data['tenant']) : null;
+    }
+
+    public function getUserTenants(): array
+    {
+        return $this->data['user_tenants'] ?? [];
+    }
+
+    public static function getTemplate(): ?string
+    {
+        return self::COLLECTOR_TEMPLATE;
+    }
+}
