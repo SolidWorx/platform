cf3dfa26547df7e971af257339d7b26db1a135ed
Pierre du Plessis <pierre@pcservice.co.za>
2026-09-08 15:14:14 +0300

    Fix PHPStan: specify TData on FormTypeInterface and inherit buildForm param types

--- src/Bundle/Platform/Form/Type/Profile/ChangePasswordType.php
+++ src/Bundle/Platform/Form/Type/Profile/ChangePasswordType.php
@@ -58,10 +58,6 @@ final class ChangePasswordType extends AbstractType
     ) {
     }
 
-    /**
-     * @param FormBuilderInterface<mixed> $builder
-     * @param array<string, mixed> $options
-     */
     #[Override]
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
